2be34ce59686ddb5187a265a7ad4e345b99c4ee5
ranatarek-rt <rana195387@feng.bu.edu.eg>
2025-02-07 09:44:43 +0200

    movie entity for database access

--- src/main/java/com/ryuu/movieReservation/model/Movie.java
+++ src/main/java/com/ryuu/movieReservation/model/Movie.java
@@ -1,4 +1,47 @@
 package com.ryuu.movieReservation.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name="movie")
 public class Movie {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @ElementCollection
+    @CollectionTable(name = "movie_genres", joinColumns = @JoinColumn(name = "movie_id"))
+    @Column(name = "genre")
+    private List<String> genres;
+
+    @ElementCollection
+    @CollectionTable(name = "movie_cast" , joinColumns = @JoinColumn(name = "movie_id"))
+    @Column(name="cast")
+    private List<String> castList;
+
+    @ElementCollection
+    @CollectionTable(name = "movie_directors", joinColumns = @JoinColumn(name = "movie_id"))
+    @Column(name = "director")
+    private List<String> directors;
+
+    @ElementCollection
+    @CollectionTable(name = "movie_showtimes", joinColumns = @JoinColumn(name = "movie_id"))
+    @Column(name = "showtime")
+    private List<LocalDateTime> showTimes;
+
+    //use longBlob for storing binary data
+    @Lob
+    @Column(name = "poster", columnDefinition = "LONGBLOB")
+    private byte[] poster;
+
 }
