759f7127d3cb5d921c5185a467d9821ac6028053
ranatarek-rt <rana195387@feng.bu.edu.eg>
2025-02-07 09:45:19 +0200

    movie repository

--- src/main/java/com/ryuu/movieReservation/repository/MovieRepo.java
+++ src/main/java/com/ryuu/movieReservation/repository/MovieRepo.java
@@ -1,4 +1,9 @@
 package com.ryuu.movieReservation.repository;
 
-public interface movieRepo {
+import com.ryuu.movieReservation.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepo extends JpaRepository<Movie,Long> {
 }
