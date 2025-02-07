1fc0bce175ae527c15d955346618982707300b32
ranatarek-rt <rana195387@feng.bu.edu.eg>
2025-02-07 09:43:25 +0200

    movie Data transfer object

--- src/main/java/com/ryuu/movieReservation/dto/MovieDto.java
+++ src/main/java/com/ryuu/movieReservation/dto/MovieDto.java
@@ -1,4 +1,23 @@
 package com.ryuu.movieReservation.dto;
 
+
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MovieDto {
+
+    private Long id;
+    private String title;
+    private String description;
+    private List<String> genres;
+    private List<String> castList;
+    private List<String> directors;
+    private List<LocalDateTime> showTimes;
+
 }
--- src/main/java/com/ryuu/movieReservation/dto/MovieRequestDto.java
+++ src/main/java/com/ryuu/movieReservation/dto/MovieRequestDto.java
@@ -1,4 +1,21 @@
 package com.ryuu.movieReservation.dto;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MovieRequestDto {
+    private String title;
+    private String description;
+    private List<String> genres;
+    private List<String> castList;
+    private List<String> directors;
+    private List<LocalDateTime> showTimes;
 }
